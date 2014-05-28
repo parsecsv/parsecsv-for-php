ed644959754f11c0f913158c147cc86e41b7961e
William Knauss <will.knauss@gmail.com>
2014-05-27 22:08:49 -0400

    update for #13
    And updated output method while I am at it.

--- parsecsv.lib.php
+++ parsecsv.lib.php
@@ -447,19 +447,23 @@ class parseCSV {
 	 * @return [string]
 	 */
 	public function output ($filename = null, $data = array(), $fields = array(), $delimiter = null) {
-		if ( empty($filename) ) {
+		if (empty($filename)) {
 			$filename = $this->output_filename;
 		}
 
-		if ( $delimiter === null ) {
+		if (is_null($delimiter)) {
 			$delimiter = $this->output_delimiter;
 		}
 
 		$data = $this->unparse($data, $fields, null, null, $delimiter);
 
-		if ( $filename !== null ) {
+		if (!is_null($filename)) {
 			header('Content-type: application/csv');
-			header('Content-Disposition: attachment; filename="'.$filename.'"');
+			header("Content-Length: ".strlen($data));
+			header('Expires: 0');
+			header('Pragma: cache');
+			header('Cache-Control: private');
+			header('Content-Disposition: attachment; filename="'.$filename.'"; modification-date="'.date('r').'";');
 			echo $data;
 		}
 
