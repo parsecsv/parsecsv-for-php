1c4bcaffa6ff93485a2dca9952adcd1ebe2fd73a
William Knauss <william.knauss@kingdom.com>
2014-06-05 16:37:32 -0400

    bootstrap update

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -3,4 +3,4 @@
 $dir = realpath(dirname(__FILE__));
 defined('BASE') OR define('BASE', realpath($dir.'/../').DIRECTORY_SEPARATOR);
 
-require_once BASE.'parseCSV.lib.php';
+require_once BASE.'parsecsv.lib.php';
