7536d55f27fcee7a8e8b5268a8d7e85b4653d1bf
Christian Bläul <blaeul@quodata.de>
2017-11-25 14:10:23 +0100

    PHPUnit: Prevent 'Cannot redeclare class' error
    The Travis environments of PHP 5.4 and 5.6 contained both the
    namespaced and the global version of the TestCase class :-/

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -5,6 +5,7 @@ defined('BASE') OR define('BASE', dirname($dir) . '/');
 
 require_once BASE . 'parsecsv.lib.php';
 
-if (class_exists('PHPUnit_Framework_TestCase')) {
+if (!class_exists('PHPUnit\Framework\TestCase')) {
+    // we run on an older PHPUnit version without namespaces.
     require_once __DIR__ . '/PHPUnit_Framework_TestCase.inc.php';
 }
