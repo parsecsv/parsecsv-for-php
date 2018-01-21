3c31b70724780d188c3a3e82c54668ad3044c266
Christian Bläul <blaeul@quodata.de>
2018-01-21 21:30:48 +0100

    Added accidentally removed regex modifier i

--- parsecsv.lib.php
+++ parsecsv.lib.php
@@ -825,7 +825,7 @@ class parseCSV {
                 $this->file = $file;
             }
 
-            if (preg_match('/\.php$/', $file) && preg_match('/<\?.*?\?>(.*)/ms', $data, $strip)) {
+            if (preg_match('/\.php$/i', $file) && preg_match('/<\?.*?\?>(.*)/ms', $data, $strip)) {
                 $data = ltrim($strip[1]);
             }
 
