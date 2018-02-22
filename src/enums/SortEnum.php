a74736d4da0cf27748402c65683a12d9a1735823
Susann Sgorzaly <sgorzaly@portrino.com>
2018-02-22 21:01:06 +0100

    init implementation of abstract enum class

--- src/enums/SortEnum.php
+++ src/enums/SortEnum.php
@@ -1,15 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: sgorzaly
- * Date: 22.02.18
- * Time: 20:24
- */
-
 namespace ParseCsv\enums;
 
 
-class SortEnum {
+class SortEnum extends AbstractEnum {
     const SORT_TYPE_REGULAR = SORT_REGULAR;
 
     const SORT_TYPE_NUMERIC = SORT_NUMERIC;
