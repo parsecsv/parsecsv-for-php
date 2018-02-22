7a3120dd28ffbbcad962ea514c2e247c450028dd
Susann Sgorzaly <sgorzaly@portrino.com>
2018-02-22 21:15:00 +0100

    added test for sort enums (todo: handle exception on test)

--- src/enums/SortEnum.php
+++ src/enums/SortEnum.php
@@ -3,6 +3,8 @@ namespace ParseCsv\enums;
 
 
 class SortEnum extends AbstractEnum {
+    const __DEFAULT = self::SORT_TYPE_REGULAR;
+
     const SORT_TYPE_REGULAR = SORT_REGULAR;
 
     const SORT_TYPE_NUMERIC = SORT_NUMERIC;
