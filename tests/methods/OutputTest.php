363347f79db28e713fe1ae750dc6ad1e5634cd6d
Fonata <blaeul@gmail.com>
2021-04-03 00:08:35 +0200

    Tests should not produce output - capture it

--- tests/methods/OutputTest.php
+++ tests/methods/OutputTest.php
@@ -14,8 +14,10 @@ class OutputTest extends TestCase {
         $csv = new Csv();
         $data = [0 => ['a', 'b', 'c'], 1 => ['d', 'e', 'f']];
         $fields = ['col1', 'col2', 'col3'];
+        ob_start();
         $output = $csv->output('test.csv', $data, $fields, ',');
         $expected = "col1,col2,col3\ra,b,c\rd,e,f\r";
+        self::assertEquals($expected, ob_get_clean());
         self::assertEquals($expected, $output);
     }
 }
