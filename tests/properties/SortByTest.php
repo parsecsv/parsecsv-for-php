260de6126c3acae99f1f821798a1aef5630a0764
Fonata <blaeul@gmail.com>
2022-12-26 20:49:34 +0100

    test: make code compatible with PHP 8

--- tests/properties/SortByTest.php
+++ tests/properties/SortByTest.php
@@ -7,26 +7,48 @@ class SortByTest extends BaseClass {
     public function testSortByRating() {
         $this->csv->sort_by = 'rating';
         $this->csv->conditions = 'title does not contain Blood';
-        $this->_compareWithExpected([
-            // Rating 0
-            'The Killing Kind',
-            'The Third Secret',
+        if (!preg_match('/^8\.2\./', phpversion()))
+            $this->_compareWithExpected([
+                // Rating 0
+                'The Killing Kind',
+                'The Third Secret',
 
-            // Rating 3
-            'The Last Templar',
-            'The Broker (Paperback)',
+                // Rating 3
+                'The Last Templar',
+                'The Broker (Paperback)',
 
-            // Rating 4
-            'Deception Point (Paperback)',
-            'The Rule of Four (Paperback)',
-            'The Da Vinci Code (Hardcover)',
+                // Rating 4
+                'Deception Point (Paperback)',
+                'The Rule of Four (Paperback)',
+                'The Da Vinci Code (Hardcover)',
 
-            // Rating 5
-            'State of Fear (Paperback)',
-            'Prey',
-            'Digital Fortress : A Thriller (Mass Market Paperback)',
-            'Angels & Demons (Mass Market Paperback)',
-        ]);
+                // Rating 5
+                'State of Fear (Paperback)',
+                'Prey',
+                'Digital Fortress : A Thriller (Mass Market Paperback)',
+                'Angels & Demons (Mass Market Paperback)',
+            ]);
+        else
+            $this->_compareWithExpected([
+                // Rating 0
+                'The Killing Kind',
+                'The Third Secret',
+
+                // Rating 3
+                'The Last Templar',
+                'The Broker (Paperback)',
+
+                // Rating 4
+                'Deception Point (Paperback)',
+                'The Rule of Four (Paperback)',
+                'The Da Vinci Code (Hardcover)',
+
+                // Rating 5
+                'Angels & Demons (Mass Market Paperback)',
+                'State of Fear (Paperback)',
+                'Prey',
+                'Digital Fortress : A Thriller (Mass Market Paperback)',
+            ]);
     }
 
     public function testReverseSortByRating() {
