6c5dea37c5a4fc756ce9639201d04d355d36bea5
Christian Bläul <christian@blaeul.de>
2026-02-02 15:58:26 +0100

    Newer Rector versions don’t have AddCoversClassAttributeRector anymore
    https://github.com/rectorphp/rector-phpunit/pull/561/

--- tests/rector.php
+++ tests/rector.php
@@ -3,14 +3,15 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\PHPUnit\CodeQuality\Rector\Class_\AddCoversClassAttributeRector;
 use Rector\PHPUnit\AnnotationsToAttributes\Rector\ClassMethod\DataProviderAnnotationToAttributeRector;
-use Rector\PHPUnit\PHPUnit60\Rector\ClassMethod\AddDoesNotPerformAssertionToNonAssertingTestRector;
 use Rector\PHPUnit\AnnotationsToAttributes\Rector\ClassMethod\DependsAnnotationWithValueToAttributeRector;
-use \Rector\Php80\Rector\Class_\AnnotationToAttributeRector;
+use Rector\PHPUnit\CodeQuality\Rector\Class_\AddCoversClassAttributeRector;
+use Rector\PHPUnit\PHPUnit60\Rector\ClassMethod\AddDoesNotPerformAssertionToNonAssertingTestRector;
 
 return static function(RectorConfig $rectorConfig): void {
-    $rectorConfig->rule(AddCoversClassAttributeRector::class);
+    if (class_exists(AddCoversClassAttributeRector::class)) {
+        $rectorConfig->rule(AddCoversClassAttributeRector::class);
+    }
     $rectorConfig->rule(DataProviderAnnotationToAttributeRector::class);
     $rectorConfig->rule(AddDoesNotPerformAssertionToNonAssertingTestRector::class);
     $rectorConfig->rule(DependsAnnotationWithValueToAttributeRector::class);
